da9f0b00c46ef0eecd017aaa6e86c93be7157e85
alex <alex@staticlibs.net>
2017-12-01 00:21:25 +0000

    browser compat

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-define(function(){var require = WILTON_requiresync;var module = {exports: {}};var exports = module.exports;
+define(function(localRequire, exports, module) { var requireOrig = require; require = localRequire;
 // Copyright Joyent, Inc. and other Node contributors.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
@@ -1599,4 +1599,4 @@ relativeTests2.forEach(function(relativeTest) {
   });
 });
 
-return module.exports;});
+require = requireOrig;});
